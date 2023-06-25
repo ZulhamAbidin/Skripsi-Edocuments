86096ace3e8b6ddc3326090a53545300ee74a3a4
ZulhamAbidin <81916530+ZulhamAbidin@users.noreply.github.com>
2023-06-26 00:25:42 +0800

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,98 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+<!doctype html>
+<html>
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Register</title>
+</head>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+<body>
+    <div class="isolate bg-white sm:py-32 md:py-0 lg:px-8">
+
+        <div class="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
+            aria-hidden="true">
+            <div class="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
+                style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
+            </div>
         </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        <div class="mx-auto max-w-2xl text-center mt-6 md:mt-10">
+            <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl px-2">LOGIN PESERTA PENGESAHAN</h2>
+            <p class="mt-2 text-sm leading-8 text-gray-600 px-10 md:px-0">Dinas Ketenagakerjaan Bidang Penempatan Tenaga
+                Kerja Dan Perluasan Kesempatan Kerja.</p>
         </div>
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-            </label>
-        </div>
+        <form method="POST" action="{{ route('login') }}"
+            class="mx-auto lg:mt-1 max-w-xl sm:py-32 md:py-14 lg:py-7 lg:px-8 px-10">
+            @csrf
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
+            @if ($errors->has('email'))
+            <div class="fixed top-0 left-0 bg-red-200 border-l-4 border-red-500 text-red-700 p-4" role="alert">
+                <p class="font-bold">Perhatian!</p>
+                <p class="text-sm">Username Atau Password Tidak Sesuai</p>
+            </div>
             @endif
 
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+            <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+
+                <div class="sm:col-span-2">
+                    <label for="email" class="block text-sm font-semibold leading-6 text-gray-900">Email</label>
+                    <div class="mt-2.5">
+                        <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus
+                            autocomplete="username"
+                            class="focus:outline-none focus:border-indigo-600 focus:ring-1 focus:ring-indigo-600 block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-inset  sm:text-sm sm:leading-6">
+                    </div>
+                </div>
+
+                <div class="sm:col-span-2">
+                    <label for="password" class="block text-sm font-semibold leading-6 text-gray-900">Password</label>
+                    <div class="mt-2.5 relative">
+                        <input id="password" type="password" name="password" required autocomplete="current-password"
+                            class="focus:outline-none focus:border-indigo-600 focus:ring-1 focus:ring-indigo-600 block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-inset  sm:text-sm sm:leading-6">
+                        <button type="button" onclick="togglePasswordVisibility()"
+                            class="absolute top-2 right-2 text-gray-500 focus:outline-none">
+                            <svg id="eye-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor" class="h-6 w-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                    d="M2 12s2-2 5-2 5 2 5 2 2 3.5 2 6.5-2 6.5-2 6.5-5 2-5 2-2-3.5-2-6.5 2-6.5 2-6.5-2-6.5-2-6.5zM15 12a3 3 0 11-6 0 3 3 0 016 0z">
+                                </path>
+                            </svg>
+                        </button>
+                    </div>
+                    @if ($errors->has('password'))
+                    <span>{{ $errors->first('password') }}</span>
+                    @endif
+                </div>
+
+                <div class="sm:col-span-2">
+                    <button type="submit"
+                        class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Submit</button>
+                </div>
+
+        </form>
+    </div>
+
+    <script>
+        function togglePasswordVisibility() {
+            const passwordInput = document.getElementById('password');
+            const eyeIcon = document.getElementById('eye-icon');
+
+            if (passwordInput.type === 'password') {
+                passwordInput.type = 'text';
+                eyeIcon.classList.remove('text-gray-500');
+                eyeIcon.classList.add('text-indigo-600');
+            } else {
+                passwordInput.type = 'password';
+                eyeIcon.classList.remove('text-indigo-600');
+                eyeIcon.classList.add('text-gray-500');
+            }
+        }
+    </script>
+
+</body>
+
+</html>
--- resources/views/auth/register-siswa.blade.php
+++ resources/views/auth/register-siswa.blade.php
@@ -1,81 +1,93 @@
+<!doctype html>
+<html>
 
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register as Siswa') }}</div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Register</title>
+</head>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register.siswa') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror"
-                                    name="name" value="{{ old('name') }}" required autofocus>
-
-                                @error('name')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Email') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                                    name="email" value="{{ old('email') }}" required>
-
-                                @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password')
-                                }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password"
-                                    class="form-control @error('password') is-invalid @enderror" name="password"
-                                    required autocomplete="new-password">
-
-                                @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm
-                                Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control"
-                                    name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+<body>
+    <div class="isolate bg-white sm:py-32 md:py-0 lg:px-8">
+        
+        <div class="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
+            aria-hidden="true">
+            <div class="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
+                style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
             </div>
         </div>
+        
+        <div class="mx-auto max-w-2xl text-center mt-6 md:mt-10">
+            <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">DAFTAR PESERTA PENGESAHAN</h2>
+            <p class="mt-2 text-sm leading-8 text-gray-600 px-10 md:px-0">Dinas Ketenagakerjaan Bidang Penempatan Tenaga Kerja Dan Perluasan Kesempatan Kerja.</p>
+        </div>
+        
+        <form method="POST" class="mx-auto lg:mt-1 max-w-xl sm:py-32 md:py-14 lg:py-7 lg:px-8 px-10" action="{{ route('register.siswa') }}">
+         @csrf
+            
+            <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+
+                <div class="sm:col-span-2">
+                    <label for="username" class="block text-sm font-semibold leading-6 text-gray-900">Username</label>
+                    <div class="mt-2.5">
+                        <input type="text" name="name" id="name" value="{{ old('name') }}" required autofocus
+                            class="@error('name') is-invalid @enderror focus:outline-none focus:border-indigo-600 focus:ring-1 focus:ring-indigo-600 block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-inset  sm:text-sm sm:leading-6">
+                    </div>
+                    @error('name')
+                    <span class="invalid-feedback text-xs text-red-500" role="alert">
+                        <strong>Username Telah Digunakan</strong>
+                    </span>
+                    @enderror
+                </div>
+
+                <div class="sm:col-span-2">
+                    <label for="email" class="block text-sm font-semibold leading-6 text-gray-900">Email</label>
+                    <div class="mt-2.5">
+                        <input type="email" name="email" id="email" value="{{ old('email') }}" required autofocus
+                            class="@error('email') is-invalid @enderror focus:outline-none focus:border-indigo-600 focus:ring-1 focus:ring-indigo-600 block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-inset  sm:text-sm sm:leading-6">
+                    </div>
+                    @error('email')
+                    <span class="invalid-feedback text-xs text-red-500" role="alert">
+                        <strong>Email Telah Digunakan</strong>
+                    </span>
+                    @enderror
+                </div>
+
+                <div>
+                    <label for="password" class="block text-sm font-semibold leading-6 text-gray-900">Password</label>
+                    <div class="mt-2.5">
+                        <input type="password" name="password" id="password" value="{{ old('password') }}" required autofocus
+                            class="@error('password') is-invalid @enderror focus:outline-none focus:border-indigo-600 focus:ring-1 focus:ring-indigo-600 block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-inset  sm:text-sm sm:leading-6">
+                    </div>
+                    @error('password')
+                    <span class="invalid-feedback text-xs text-red-500" role="alert">
+                        <strong>Pastikan Sesuai Dengan Konfirmasi Password</strong>
+                    </span>
+                    @enderror
+                </div>
+
+                <div>
+                    <label for="password_confirmation" class="block text-sm font-semibold leading-6 text-gray-900">Konfirmasi Password</label>
+                    <div class="mt-2.5">
+                        <input type="password_confirmation" name="password_confirmation" id="password_confirmation" value="{{ old('password_confirmation') }}" required autofocus
+                            class="@error('password_confirmation') is-invalid @enderror focus:outline-none focus:border-indigo-600 focus:ring-1 focus:ring-indigo-600 block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-inset  sm:text-sm sm:leading-6">
+                    </div>
+                    @error('password_confirmation')
+                    <span class="invalid-feedback text-xs text-red-500" role="alert">
+                        <strong>Pastikan Sesuai Dengan Konfirmasi Password</strong>
+                    </span>
+                    @enderror
+                </div>
+                
+                <div class="sm:col-span-2">
+                    <button type="submit"
+                        class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Submit</button>
+                </div>
+
+        </form>
     </div>
-</div>
+
+</body>
+
+</html>
